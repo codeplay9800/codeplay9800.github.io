1df727daf6767c604ec23fc1995cb27b5c642bb7
Shubham Sharma <sharmashubham9800@gmail.com>
2025-04-14 01:41:17 -0700

    Updated PBR Project

--- images/Projects/565/halfTIme/webm_5S.sh
+++ images/Projects/565/halfTIme/webm_5S.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for f in *.webm; do
   # Trim video to first 5 seconds and save with _5s in name
-  ffmpeg -y -i "$f" -t 2.5 -c:v libvpx-vp9 -crf 30 -b:v 0 "${f%.webm}_5s.webm"
+  ffmpeg -y -i "$f" -t 3 -c:v libvpx-vp9 -crf 30 -b:v 0 "${f%.webm}_5s.webm"
 done
